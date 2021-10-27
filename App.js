4a8407ccd03f08bddde0b0077be82aa9f9b51fc8
Loushik LK <loushikkumargiri@gmail.com>
2021-10-27 15:17:19 +0530

    react-native

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screen/home/HomeScreen'
+import ChatScreen from './src/screen/chat/ChatScreen'
+import SearchScreen from './src/screen/search/SearchScreen'
+import ProfileScreen from './src/screen/profile/Profile'
+import Login from './src/screen/Login'
+import { Ionicons } from '@expo/vector-icons'
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [isSignIn, setIsSignIn] = useState(true)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    isSignIn ? (
+      <>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === "Home") {
+                  iconName = focused ? "home" : "home-outline"
+                }
+                else if (route.name === 'Chat') {
+                  iconName = focused ? 'chatbubble' : 'chatbubbles-outline';
+                }
+                else if (route.name === 'Search') {
+                  iconName = focused ? 'search' : 'search';
+                }
+                else if (route.name === 'Profile') {
+                  iconName = focused ? 'person' : 'person-outline';
+                }
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: '#3370ff',
+              tabBarInactiveTintColor: '#3370ff',
+              tabBarStyle: { backgroundColor: "#0f0f0f", paddingTop: 10, paddingBottom: 10, height: 60 },
+
+            })}
+            backBehavior="history"
+            sceneContainerStyle={styles.container}
+
+          >
+
+            <Tab.Screen name="Home" component={HomeScreen} options={{ title: "Home" }} />
+            <Tab.Screen name="Chat" component={ChatScreen} />
+            <Tab.Screen name="Search" component={SearchScreen} />
+            <Tab.Screen name="Profile" component={ProfileScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </>) : (
+      <Login />
+    )
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    backgroundColor: '#f0f00f',
+    color: "#ffffff"
   },
 });
